8fb32abbaf54a8ee562e84a86e711ce619d9e2e8
cc-drake <cc-dr@Drake-PC>
2021-02-21 09:14:02 +0100

    Projekt angelegt

---
+++ src/de/drake/cantstop/Launcher.java
@@ -0,0 +1,17 @@
+package de.drake.cantstop;
+
+import de.drake.cantstop.view.UI;
+
+public class Launcher {
+	
+	/**
+	 * Startet das Programm.
+	 * 
+	 * @param args
+	 * 		wird ignoriert
+	 */
+	public static void main(String[] args) {
+		new UI();
+	}
+	
+}
---
+++ src/de/drake/cantstop/view/UI.java
@@ -0,0 +1,5 @@
+package de.drake.cantstop.view;
+
+public class UI {
+
+}
